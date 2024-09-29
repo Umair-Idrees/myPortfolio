57a1272c36896404b46334f09f4fb6a09284566e
Muhammad Umair Idrees <umairtech607@gmail.com>
2024-09-29 20:20:09 +0500

    add

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../Portfolio/Portfolio.css";
 import portfolio1 from "../../assets/portfolio1.png";
-import portfolio2 from "../../assets/Portfolio2.png";
-import portfolio3 from "../../assets/Portfolio3.png";
-import portfolio4 from "../../assets/Portfolio4.png";
-import portfolio5 from "../../assets/Portfolio5.png";
-import portfolio6 from "../../assets/Portfolio6.png";
+import portfolio2 from "../../assets/portfolio2.png";
+import portfolio3 from "../../assets/portfolio3.png";
+import portfolio4 from "../../assets/portfolio4.png";
+import portfolio5 from "../../assets/portfolio5.png";
+import portfolio6 from "../../assets/portfolio6.png";
 
 const Portfolio = () => {
   return (
@@ -22,7 +22,6 @@ const Portfolio = () => {
         <img src={portfolio4} alt="" className="worksImg" />
         <img src={portfolio6} alt="" className="worksImg" />
         <img src={portfolio5} alt="" className="worksImg" />
-        
       </div>
       <button className="worksbtn">See More</button>
     </section>
